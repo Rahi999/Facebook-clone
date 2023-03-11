1a789bd170a9363cdf675162f5d3ea7a558ab7b2
Rahi999 <rahimansari4560@gmail.com>
2023-03-11 19:29:31 +0530

    backend: edit user's personal profile done

--- server-codeBase/routes/userAuth.js
+++ server-codeBase/routes/userAuth.js
@@ -1,38 +1,13 @@
 const express = require("express")
-const {SignUp, Login} = require("../controllers/userAuth")
+const {SignUp, Login, getUsers, editUser, deleteUser} = require("../controllers/userAuth")
 const {getData, postDemoData} = require("../controllers/AuthDemo")
 const userRouter = express.Router()
-const {userModel, UserSchema} = require("../models/userAuth")
-const {AuthdemoSchema, authdemoModel} = require("../models/AuthDemo");
 
-userRouter.get("/", async (req,res) => {
-    try{
-        const allUsers = await userModel.find({})
-        res.send(allUsers)
-    }
-    catch (error){
-        res.send("✖ Something went wrong!!")
-    }
-})
-
+userRouter.get("/", getUsers )
 userRouter.post("/signup", SignUp)
 userRouter.post("/signin", Login)
-userRouter.delete("/delete/:id", async (req, res) => {
-    const ID = req.params.id;
-    const user = await userModel.find({"_id":ID});
-    try{
-        if(!user){
-            res.send("User not found with this id")
-        } else{
-            await userModel.findOneAndDelete({"_id":ID});
-            res.send("User deleted")
-        }
-
-    }
-    catch(error){
-        console.log(error)
-    }
-})
+userRouter.patch("/editUserProfile/:id", editUser)
+userRouter.delete("/delete/:id", deleteUser)
 userRouter.get("/getDemoDatas", getData);
 userRouter.post("/postDemoDatas", postDemoData);
 module.exports = userRouter
