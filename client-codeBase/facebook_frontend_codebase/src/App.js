9769497d4f533e66bdf28c8c8afc13df3bc61756
rahim8masai <rahim.ansari@masaischool.com>
2023-05-18 20:07:53 +0530

    Basic UI changes

--- client-codeBase/facebook_frontend_codebase/src/App.js
+++ client-codeBase/facebook_frontend_codebase/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider, Input, Button } from '@chakra-ui/react'
+import { ChakraProvider, Input, Button, Avatar } from '@chakra-ui/react'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <ChakraProvider>
        <h1>Create Facebook</h1>
+       <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/800px-Facebook_logo_%28square%29.png" />
        <Input placeholder='Enter name...' w="50%"/>
        <Button>Login</Button>
       </ChakraProvider>
