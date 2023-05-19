8e29962acf3763c36454c09f4b220b15040fd455
rahim8masai <rahim.ansari@masaischool.com>
2023-05-19 07:40:28 +0530

    Added login route

--- client-codeBase/facebook_frontend_codebase/src/Routes/AllRoutes.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route, Routes} from "react-router-dom"
 import SideBar from '../Components/SideBar.jsx'
+import Login from '../Pages/Login.jsx'
 
 const AllRoutes = () => {
   return (
     <div>
         <Routes>
         <Route path="/" element={<SideBar />} />
+        <Route path="/login" element={<Login />} />
         </Routes>
     </div>
   )
