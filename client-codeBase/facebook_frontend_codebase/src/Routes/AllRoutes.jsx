17cf49c9b3bc1d0a2e51b2c022993b5a9a86a51e
rahim8masai <rahim.ansari@masaischool.com>
2023-05-19 07:41:12 +0530

    Added signup route

--- client-codeBase/facebook_frontend_codebase/src/Routes/AllRoutes.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Routes} from "react-router-dom"
 import SideBar from '../Components/SideBar.jsx'
 import Login from '../Pages/Login.jsx'
+import SignUp from '../Pages/SignUp.jsx'
 
 const AllRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AllRoutes = () => {
         <Routes>
         <Route path="/" element={<SideBar />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
         </Routes>
     </div>
   )
