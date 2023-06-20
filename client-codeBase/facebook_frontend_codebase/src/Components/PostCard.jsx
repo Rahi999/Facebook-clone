c71456ee0ef48835d5a7d1427a0490fcf7c26890
rahim8masai <rahim.ansari@masaischool.com>
2023-06-20 16:37:16 +0530

    Dislike done

--- client-codeBase/facebook_frontend_codebase/src/Components/PostCard.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Components/PostCard.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { Box, Text, Button, Image, Tooltip, AvatarGroup, Drawer, Divider, Flex } from "@chakra-ui/react"
+import axios from "axios";
+import { Box, Text, Button, Image, Tooltip, AvatarGroup, Drawer, Divider, Flex, useToast } from "@chakra-ui/react"
 import { IconButton } from '@chakra-ui/react';
 import { BiCommentDetail } from 'react-icons/bi';
-import { AiOutlineDislike } from 'react-icons/ai';
+import { AiFillDislike, AiOutlineDislike } from 'react-icons/ai';
 import { Link, useNavigate } from "react-router-dom";
 import "./Post.css";
 import LikeButton from "./Like";
 import CreatePost from "./CreatePost";
+import { getCookies } from "../utils/getData";
 // import "./ImageInput.css";
 const PostCard = ({
     user_profile,
@@ -18,12 +20,44 @@ const PostCard = ({
     userId
 }) => {
 
+    const toast = useToast()
+    const [loading, setLoading] = useState(false)
+    const [disliked, setDisLiked] = useState(false)
     const navigate = useNavigate()
     const [selectedImage, setSelectedImage] = useState(null);
+    const token = getCookies("fb_token")
+
     const handleImageChange = (event) => {
         const file = event.target.files[0];
         setSelectedImage(URL.createObjectURL(file));
     };
+
+    const handleDislike = () => {
+        if (userId && PostId) {
+            console.log(userId, PostId)
+            setLoading(true)
+            const payload = { dislikerId: userId }
+            axios.put(`${process.env.REACT_APP_DEV_BASE_URL}/post/dislike/${PostId}`, payload, { headers: { "Authorization": `${token}` } })
+                .then((res) => {
+                    setLoading(false)
+                    setTimeout(() => {
+                        setDisLiked(true)
+                    }, 100)
+                })
+                .catch((err) => {
+                    setLoading(false)
+                    toast({
+                        title: err.response.data.message,
+                        position: "top",
+                        status: 'info',
+                        duration: 3000,
+                    })
+                    setTimeout(() => {
+                        setDisLiked(true)
+                    }, 100)
+                })
+        }
+    }
     return (
         <Box className="post"
         >
@@ -41,33 +75,36 @@ const PostCard = ({
                 </Box>
             </Box>
             <Box className="post-content">
-                <Text className="post-text" fontSize={{base: "14px", sm:"14px", md: "16px", lg:"17px", xl: "18px"}} maxW={'100%'}>{post_text}</Text>
+                <Text className="post-text" fontSize={{ base: "14px", sm: "14px", md: "16px", lg: "17px", xl: "18px" }} maxW={'100%'}>{post_text}</Text>
                 <Image className="post-image" src={post_image} alt="" />
             </Box>
-            
+
             {/* <Box className="post-actions"> */}
-            
+
             <LikeButton postId={PostId} />
-            <Box  mt="-10" width={{base: "70%", sm: '70%', md: "55%", lg: "55%", xl: "55%"}} display="flex" textAlign="right" ml={{ base: "30%", sm: "30%", md: "30%", lg: "40%", xl: '40%' }} justifyContent={'space-between'}>
+            <Box mt="-10" width={{ base: "70%", sm: '70%', md: "55%", lg: "55%", xl: "55%" }} display="flex" textAlign="right" ml={{ base: "30%", sm: "30%", md: "30%", lg: "40%", xl: '40%' }} justifyContent={'space-between'}>
                 {/* <Button width={{ base: "70%", sm: "70%", md: "70%", lg: "70%", xl: "30%" }} fontSize={{ base: "12px", sm: "14px", md: "", lg: "", xl: "" }} className="post-share">Share</Button> */}
-                <Tooltip label="Dislike">
-        <IconButton
-          aria-label="Dislike"
-          icon={<AiOutlineDislike />}
-        //   colorScheme="blue"
-        //   ml={4}
-        />
-      </Tooltip>
+                <Tooltip label="Dislike" >
+                    <IconButton
+                                isDisabled={disliked}
+                        onClick={handleDislike}
+                        aria-label="Dislike"
+                        // icon={<AiOutlineDislike />}
+                        icon={ disliked ? <AiFillDislike /> : <AiOutlineDislike />}
+                    //   colorScheme="blue"
+                    //   ml={4}
+                    />
+                </Tooltip>
                 {/* <Button width={{ base: "70%", sm: "70%", md: "70%", lg: "70%", xl: "30%" }} fontSize={{ base: "12px", sm: "14px", md: "", lg: "", xl: "" }} className="post-comment">Comment</Button> */}
                 <Tooltip label="Comment">
-        <Link to={`/comments/${PostId}`}>
-        <IconButton
-          aria-label="Comment"
-          icon={<BiCommentDetail />}
-        //   colorScheme="blue"
-        //   ml={4}
-        /></Link>
-      </Tooltip>
+                    <Link to={`/comments/${PostId}`}>
+                        <IconButton
+                            aria-label="Comment"
+                            icon={<BiCommentDetail />}
+                        //   colorScheme="blue"
+                        //   ml={4}
+                        /></Link>
+                </Tooltip>
             </Box>
             {/* </Box> */}
         </Box>
