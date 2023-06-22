14f707de43b6df07f22633f909e80b89700d6b5e
rahim8masai <rahim.ansari@masaischool.com>
2023-06-22 17:13:50 +0530

    comment initiated

---
+++ client-codeBase/facebook_frontend_codebase/src/Components/AllComments.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const AllComments = ({ }) => {
+    return (<>
+        <Box>
+            AllComments
+        </Box>
+    </>)
+}
+export default AllComments
