a7d69717d202091056e38950d78ba304d728798d
rahim8masai <rahim.ansari@masaischool.com>
2023-06-22 18:11:32 +0530

    comment done

---
+++ client-codeBase/facebook_frontend_codebase/src/Components/AddComment.jsx
@@ -0,0 +1,96 @@
+import { Avatar, Box, Button, Flex, Input, useToast } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { getCookies } from "../utils/getData";
+import axios from "axios";
+
+const AddComment = ({postId, getPost}) => {
+
+  const [loading, setLoading] = useState(false)
+  const [newComment, setNewComment] = useState("");
+  const toast = useToast()
+  const userId = getCookies("userId")
+  const token = getCookies('fb_token')
+
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const options = {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    };
+    const formattedDateTime = currentDateTime.toLocaleString('en-US', options);
+
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const handleAddComment = () => {
+    if(newComment){
+      setLoading(true)
+        const payload = {
+            comment: newComment,
+            postId: postId,
+            user: userId,
+            commentAt: formattedDateTime
+        }
+        axios.put(`${process.env.REACT_APP_DEV_BASE_URL}/post/comment`, payload, { headers: { "Authorization": `${token}` } })
+        .then((res) => {
+          getPost()
+          setLoading(false)
+        })
+        .catch((err) => {
+            toast({
+                description: err.response.data.message,
+                position: "top",
+                status: "error",
+                duration: 3000,
+                isClosable: true
+            })
+            setLoading(false)
+        })
+    }
+    else{
+        toast({
+            description: "Can't create empty comment",
+            position: "top",
+            status: "info",
+            duration: "3000",
+            isClosable: false
+        })
+    }
+    
+  };
+    
+    return (
+        <>
+        <Box>
+        <Flex mb={4}>
+        <Avatar size="sm" mr={2} />
+        <Input
+          flex="1"
+          bg="white"
+          borderRadius="full"
+          borderWidth="1px"
+          borderColor="gray.200"
+          placeholder="Write a comment..."
+          value={newComment}
+          onChange={handleCommentChange}
+        />
+        <Button
+          colorScheme="blue"
+          borderRadius="full"
+          px={6}
+          ml={4}
+          onClick={handleAddComment}
+          disabled={loading}
+        >
+          {loading ? "Commenting..." : "Comment"}
+        </Button>
+      </Flex>
+        </Box>
+        </>
+    )
+}
+export default AddComment
--- client-codeBase/facebook_frontend_codebase/src/Components/CommentsList.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Components/CommentsList.jsx
@@ -5,10 +5,13 @@ import axios from "axios";
 import { getCookies } from "../utils/getData";
 import { useNavigate } from "react-router-dom";
 import PostCard from "./PostCard";
+import AllComments from "./AllComments";
+import AddComment from "./AddComment";
 
 const CommentsList = ({ postId }) => {
     const [post, setPost] = useState(null);
     const [loading, setLoading] = useState(false)
+    const [comments, setComments] = useState(null)
     const token = getCookies("fb_token")
     const navigate = useNavigate()
 
@@ -18,6 +21,7 @@ const CommentsList = ({ postId }) => {
             .then((res) => {
                 console.log(res.data)
                 setPost(res.data)
+                setComments(res.data.comments.reverse())
                 setLoading(false)
             })
             .catch((err) => {
@@ -28,10 +32,13 @@ const CommentsList = ({ postId }) => {
 
     useEffect(() => {
         getPost()
+        // console.log(post.comments)
     }, [])
 
     return loading ? (<Loading />) : (<>
-        <Box mt="20" pr={{base: "0", sm: "0", md: "30", lg: "80", xl: "80"}} >
+        <Box mt="20" pr={{base: "0", sm: "0", md: "30", lg: "80", xl: "80"}} 
+        pb="40"
+        >
             {post && <PostCard
                         user_profile={post.user.profile_pic}
                         user_name={post.user.firstname + post.user.surename}
@@ -41,6 +48,15 @@ const CommentsList = ({ postId }) => {
                         PostId={post._id}
                         userId={post.user._id}
                     />}
+                    <AddComment postId={postId} getPost={getPost} />
+                    {comments && comments.map((el) => <AllComments
+                     id = {el._id}
+                     userId = {el.user._id}
+                     comment={el.comment}
+                     date = {el.commentAt}
+                     username = {el.user.firstname + el.user.surename}
+                     profile_pic = {el.user.profile_pic}
+                     />)}
         </Box>
     </>)
 }
