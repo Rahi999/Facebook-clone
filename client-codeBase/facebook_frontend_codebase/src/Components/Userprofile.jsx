3fb0e993839e30e78cf969f773ad94ce16ba0f30
rahim8masai <rahim.ansari@masaischool.com>
2023-06-23 07:37:44 +0530

    User profile created

---
+++ client-codeBase/facebook_frontend_codebase/src/Components/Userprofile.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import {
+    Heading,
+    Avatar,
+    AvatarBadge,
+    Box,
+    Center,
+    Image,
+    Flex,
+    Text,
+    Stack,
+    Button,
+    useColorModeValue,
+    IconButton,
+    Toast,
+    useToast
+} from '@chakra-ui/react';
+import { SmallCloseIcon, EditIcon } from '@chakra-ui/icons';
+import { getCookies } from "../utils/getData";
+import Loading from "./Loading";
+
+const UserProfile = () => {
+
+    const [userData, setUserData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const toast = useToast()
+    const params = useParams()
+    const userId = params.userId
+    const id = getCookies('userId')
+    // console.log(userId, id)
+    const token = getCookies('fb_token')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (userId && token) {
+            setLoading(true)
+            axios.get(`${process.env.REACT_APP_DEV_BASE_URL}/profile/getSingleUser/${userId}`, { headers: { "Authorization": `${token}` } })
+                .then((res) => {
+                    setUserData(res.data)
+                    setLoading(false)
+                    console.log(userData)
+                })
+                .catch((err) => {
+                    setLoading(false)
+                    toast({
+                        title: "User not found",
+                        position: "top",
+                        status: 'error',
+                        duration: 3000,
+                    })
+                    navigate("/")
+                })
+        }
+        else {
+            navigate("/login")
+        }
+    }, [])
+    return userData ? (<Center py={6}><Box
+        maxW={'300px'}
+        w={'full'}
+        // bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        overflow={'hidden'}>
+        <Image
+            h={'120px'}
+            w={'full'}
+            src={userData.cover_pic}
+            objectFit={'cover'}
+        />
+        <Flex justify={'center'} mt={-12}>
+            <Avatar
+                size={'xl'}
+                src=''
+                name={userData.firstname + userData.surename}
+                alt={'User_image'}
+
+            >
+                <AvatarBadge
+                    as={IconButton}
+                    size="xs"
+                    rounded="full"
+                    border="0px"
+                    top="50px"
+                    colorScheme="blue"
+                    aria-label="edit Image"
+                    icon={<EditIcon />}
+                />
+            </Avatar>
+        </Flex>
+
+        <Box p={6}>
+            <Stack spacing={0} align={'center'} mb={5}>
+                <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                    {userData.firstname + " " + userData.surename}
+                </Heading>
+                <Text color={'gray.500'}>{userData.email[0] + userData.email[1] + userData.email[2] + "******@gmail.com"}</Text>
+            </Stack>
+
+            <Stack direction={'row'} justify={'center'} spacing={6}>
+                <Stack spacing={0} align={'center'}>
+                    <Text fontWeight={600}>{userData.followers.length}</Text>
+                    <Text fontSize={'sm'} color={'gray.500'}>
+                        Followers
+                    </Text>
+                </Stack>
+                <Stack spacing={0} align={'center'}>
+                    <Text fontWeight={600}>{userData.following.length}</Text>
+                    <Text fontSize={'sm'} color={'gray.500'}>
+                        Followeing
+                    </Text>
+                </Stack>
+            </Stack>
+
+            <Button
+                marginTop="10%"
+                width="70%"
+                flex={1}
+                fontSize={'sm'}
+                rounded={'full'}
+                bg={'blue.400'}
+                color={'white'}
+                boxShadow={
+                    '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+                }
+                _hover={{
+                    bg: 'blue.500',
+                }}
+                _focus={{
+                    bg: 'blue.500',
+                }}>
+                Follow
+            </Button>
+        </Box>
+    </Box></Center>) : (<Loading />)
+
+
+}
+export default UserProfile
