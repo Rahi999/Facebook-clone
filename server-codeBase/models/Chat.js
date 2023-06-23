1f5e8876225ce9fd79dcb21f7198e68476ee40f6
rahim8masai <rahim.ansari@masaischool.com>
2023-06-23 13:51:49 +0530

    chat schema initiated

---
+++ server-codeBase/models/Chat.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const chatSchema = new mongoose.Schema({
+    senderId: { type: String, required: true },
+    receiverId: { type: String, required: true },
+    message: { type: String, required: true },
+  });
+  
+  module.exports = mongoose.model('Message', chatSchema);
