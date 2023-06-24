64406f1134dc528ca2a7864b76994a6356df3b90
rahim8masai <rahim.ansari@masaischool.com>
2023-06-24 08:25:56 +0530

    messages initiated

--- client-codeBase/facebook_frontend_codebase/src/Components/Chat.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Components/Chat.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Box } from "@chakra-ui/react"
+import { Box, VStack, Avatar, Text } from '@chakra-ui/react';
+import ChatLists from "./ChatLists";
 
 const Chat = () => {
+
+    const users = [
+        { id: "1", firstname: "Name", surename: "lastname", profile_pic: "demo_img.png" },
+        { id: "2", firstname: "Name", surename: "lastname", profile_pic: "demo_img.png" }
+    ]
     return (<>
         <Box>
-            Chat
+            <ChatLists users={users} />
+           
         </Box>
     </>)
 }
--- client-codeBase/facebook_frontend_codebase/src/Components/ChatLists.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Components/ChatLists.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import {useParams} from "react-router-dom"
+import axios from "axios"
+import { Box, VStack, Avatar, Text, IconButton } from '@chakra-ui/react';
+import { BiCommentDetail } from "react-icons/bi";
+import Follow from "./Follow";
 
-const ChatLists = () => {
+const ChatLists = ({users}) => {
+    const params = useParams()
+    // const senderId = params.serderId;
+    // const receiverId = params.receiverId;
+
+    const senderId = "6495c422e94b5545318eb3eb";
+    const receiverId = "6495c422e94b5545318eb3ec";
+    
     return (<>
         <Box>
-            ChatLists
+            <VStack align="start" spacing={4}>
+                {users.map((user) => (
+                    <Box key={user.id} display="flex" alignItems="center">
+                        <Avatar src={user.profile_pic} />
+                        <Text ml={2}>{`${user.firstname} ${user.surename}`}</Text>
+                        <IconButton
+                            aria-label="Comment"
+                            icon={<BiCommentDetail />}
+                        />
+                    </Box>
+                ))}
+            </VStack>
         </Box>
     </>)
 }
