e4b5481a7e9f1f2120d0f7aee471e6f65e986617
rahim8masai <rahim.ansari@masaischool.com>
2023-06-24 16:32:26 +0530

    css Changes

--- client-codeBase/facebook_frontend_codebase/src/Components/Messages.jsx
+++ client-codeBase/facebook_frontend_codebase/src/Components/Messages.jsx
@@ -1,5 +1,5 @@
 import { Box, VStack, Avatar, Text, Input, Button, Flex, Toast, useToast } from '@chakra-ui/react';
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import SideBar from './SideBar';
 import { getCookies } from '../utils/getData';
 import axios from 'axios';
@@ -12,11 +12,13 @@ const Messages = ({ senderId, receiverId }) => {
   const [user, setUser] = useState(null)
   const [messages, setMessages] = useState([])
   const [loading, setLoading] = useState(false)
+  const boxRef = useRef(null);
   const userId = getCookies("userId")
   const token = getCookies("fb_token")
   const params = useParams()
   const toast = useToast()
   const navigate = useNavigate()
+  const theme = localStorage.getItem('chakra-ui-color-mode')
 
   const handleInputChange = useCallback((e) => {
     setText(e.target.value);
@@ -46,6 +48,7 @@ const Messages = ({ senderId, receiverId }) => {
         { headers: { "Authorization": `${token}` } })
         .then((res) => {
           console.log(res.data)
+          setText("")
           getMessages()
         })
         .catch((err) => console.log(err))
@@ -61,6 +64,18 @@ const Messages = ({ senderId, receiverId }) => {
     }
 
   }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSendMessages();
+    }
+  };
+
+  const handleScrollToBottom = () => {
+    if (boxRef.current) {
+      boxRef.current.scrollTop = boxRef.current.scrollHeight;
+    }
+  };
 
   useEffect(() => {
     if(userId && params.receiverId){
@@ -95,31 +110,48 @@ const Messages = ({ senderId, receiverId }) => {
             position="relative"
           >
             {/* Header */}
-            <Flex borderRadius={'8px'} onClick={() => navigate(`/users-profile/${params.receiverId}`)} gap='2' p='2' align="center" width="100%" boxShadow={'rgba(0, 0, 0, 0.24) 0px 3px 8px'} cursor={'pointer'}>
+            <Flex position={'sticky'} onClick={() => navigate(`/users-profile/${params.receiverId}`)} gap='2' p='2' align="center" width="100%" boxShadow={'rgba(0, 0, 0, 0.16) 0px 2px 6px'} cursor={'pointer'}>
               <Avatar src={user && user.profile_pic} size="sm" />
               <Text fontWeight="bold">{user && user.firstname + " " + user.surename}</Text>
               {/* <Avatar src={messages && messages[0]?.profile_pic} size="sm" /> */}
             </Flex>
 
             {/* Chat messages */}
-            <VStack width="100%" mt='3' align="flex-start" spacing={2} overflowY="auto" flex="1" pb={16} p='2' boxShadow={'rgba(0, 0, 0, 0.24) 0px 3px 8px'}>
+            <VStack
+            ref={boxRef}
+            width="100%" align="flex-start" spacing={2} overflowY="auto" flex="1" pb={16}
+            p='2' boxShadow={'rgba(0, 0, 0, 0.24) 0px 3px 8px'}>
               {messages && messages.map((message) => (
-                <Flex key={message.id} justify={message.senderId == userId ? 'flex-end' : 'flex-start'} width="100%">
+                <Flex key={message.id} justify={message.senderId == userId ? 'flex-end' : 'flex-start'} width="100%" gap={'1'}>
                   {message.senderId !== userId && (
-                    <Avatar src={getCookies('user-profile')} size="sm" display={{ base: 'none', md: 'inline' }} />
+                    <Avatar
+                    cursor={'pointer'} 
+                    onClick={() => navigate(`/users-profile/${params.receiverId}`)}
+                    src={user.profile_pic} size="sm" display={{ base: 'none', md: 'inline' }} />
                   )}
                   <Box className='box'
-                    bg={message.senderId === userId ? 'white' : 'blue.500'}
-                    color={message.senderId === userId ? 'gray.800' : 'white'}
-                    p={2}
-                    borderRadius="md"
+                  mt={'5'}
+                  borderTopRightRadius={message.senderId !== userId ? "20px" : "0px"}
+                  borderBottomRightRadius={message.senderId !== userId ? "20px" : "20px"}
+                  borderBottomLeftRadius={message.senderId !== userId ? "20px" : "20px"}
+                  borderTopLeftRadius={message.senderId === userId ? "20px" : "0px"}
+                    bg={ message.senderId === userId ? theme == "dark" ? 'grey' : "white" : 'blue.500'}
+                    color={message.senderId === userId ? theme == "dark" ? "white" : 'gray.800' : 'white'}
+                    p={2.5}
+                    // borderRadius="md"
                     boxShadow="md"
-                    maxWidth="80%"
+                    maxWidth="90%"
                   >
                     <Text>{message.message}</Text>
                   </Box>
                   {message.senderId === userId && (
-                    <Avatar src={user.profile_pic} size="sm" display={{ base: 'none', md: 'inline' }} />
+                    <Avatar  
+                    cursor={'pointer'}
+                    onClick={() => navigate(`/users-profile/${userId}`)}
+                    src={getCookies('user-profile')}
+                     size="sm"
+                      display={{ base: 'none', md: 'inline' }}
+                       />
                   )}
                 </Flex>
               ))}
@@ -129,7 +161,12 @@ const Messages = ({ senderId, receiverId }) => {
             <Box position="sticky" bottom={0} width="100%" p={4} backdropFilter="blur(8px)"
             >
               <Flex align="center">
-                <Input placeholder="Type a message" flex="1" mr={2} value={text} onChange={handleInputChange} />
+                <Input placeholder="Type a message"
+                 flex="1"
+                  mr={2}
+                   value={text}
+                   onKeyDown={handleKeyDown}
+                   onChange={handleInputChange} />
                 <Button colorScheme="blue" onClick={() => handleSendMessages()}>Send</Button>
               </Flex>
             </Box>
