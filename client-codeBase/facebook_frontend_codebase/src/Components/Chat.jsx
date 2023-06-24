70381f19b9c20b9cf01ad1affde61d28e84c6199
rahim8masai <rahim.ansari@masaischool.com>
2023-06-24 07:57:59 +0530

    Chat frontend initiated

---
+++ client-codeBase/facebook_frontend_codebase/src/Components/Chat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box } from "@chakra-ui/react"
+
+const Chat = () => {
+    return (<>
+        <Box>
+            Chat
+        </Box>
+    </>)
+}
+export default Chat
