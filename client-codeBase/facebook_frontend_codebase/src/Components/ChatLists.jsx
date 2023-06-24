c80a7b5ad4dd0141c57130ccc7b63b0a81bdc912
rahim8masai <rahim.ansari@masaischool.com>
2023-06-24 07:59:03 +0530

    ChatLists frontend initiated

---
+++ client-codeBase/facebook_frontend_codebase/src/Components/ChatLists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const ChatLists = () => {
+    return (<>
+        <Box>
+            ChatLists
+        </Box>
+    </>)
+}
+export default ChatLists
